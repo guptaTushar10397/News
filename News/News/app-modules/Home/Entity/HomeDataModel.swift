c64d6206e601ad0c616bfc9d85c2aff5fad3efaa
Tushar Gupta <tushar.gupta@noice.id>
2024-08-22 16:34:49 +0530

    code data code added.

--- News/News/app-modules/Home/Entity/HomeDataModel.swift
+++ News/News/app-modules/Home/Entity/HomeDataModel.swift
@@ -18,28 +18,27 @@ struct Response: Codable {
 struct Docs: Codable {
     var abstract: String?
     var multimedia: [Multimedia]?
-    var headline: Headline?
     var pubDate: String?
     
     private enum CodingKeys: String, CodingKey {
         case abstract
         case multimedia
-        case headline
         case pubDate = "pub_date"
+    }
+    
+    init(abstract: String?, multimedia: [Multimedia]?, pubDate: String?) {
+        self.abstract = abstract
+        self.multimedia = multimedia
+        self.pubDate = pubDate
     }
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.abstract = try container.decodeIfPresent(String.self, forKey: .abstract)
         self.multimedia = try container.decodeIfPresent([Multimedia].self, forKey: .multimedia)
-        self.headline = try container.decodeIfPresent(Headline.self, forKey: .headline)
         self.pubDate = try container.decodeIfPresent(String.self, forKey: .pubDate)
     }
     
-}
-
-struct Headline: Codable {
-    var main: String?
 }
 
 struct Multimedia: Codable {
